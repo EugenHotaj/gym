59187a7b8907044b03b2e345b18acbbec64ad261
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2018-03-29 18:13:20 -0400

    Progress on SnakeEnv.

--- envs/gym-snake/README.md
+++ envs/gym-snake/README.md
@@ -1,1 +1,15 @@
-# TODO
+# gym-snake
+A Snake environment bundle for OpenAI Gym.
+
+## Installation
+```shell
+    pip install -e .
+```
+
+## Usage
+```python
+    import gym
+    import gym-snake
+
+    env = gym.make('Snake-v0')
+```
--- envs/gym-snake/gym_snake/envs/snake_env.py
+++ envs/gym-snake/gym_snake/envs/snake_env.py
@@ -9,7 +9,32 @@ import gym
 from gym import error
 from gym import spaces
 from gym import utils
+import numpy as np
 import pygame
+
+
+class Game(object):
+  def __init__(self, width=20, height=20, snake_size=5):
+    self._board = np.array((width, height))
+    self._snake = self._generate_initial_snake(size=snake_size)
+    while True:
+      x, y = (np.random.rand_int(0, width), np.random.randint(height))
+      self._apple = np.array([x, y])
+      if x not in self._snake[:, 0] or y not in self._snake[:, 1]:
+        break
+
+  def _generate_initial_snake(self, size=5):
+    snake = np.array(size, 2)
+    # Subtract size from x-bounds because we always extends snake to the right.
+    snake[0, 0] = np.random.randint(0, self._board.shape[0] - size)
+    snake[0, 1] = np.random.randint(0, self._board.shape[1])
+
+    # TODO(EugenHotaj): snake should generate in random configurations.
+    for i in range(1, size):
+      snake[i, 0] = snake[i-1, 0] + 1
+      snake[i, 1] = snake[i-1]
+
+    return snake
 
 
 class SnakeEnv(gym.Env):
