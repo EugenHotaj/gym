1a21f1ac4118b884642f401f2c9f07488099b26f
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2018-04-15 14:52:24 -0400

    some clean up

--- envs/gym-snake/gym_snake/envs/snake_env.py
+++ envs/gym-snake/gym_snake/envs/snake_env.py
@@ -15,7 +15,68 @@ from gym import logger
 from gym import spaces
 import numpy as np
 import pygame
-from .game import game
+
+_DIRS = [
+    (1, 0),  # Right
+    (0, -1),  # Up
+    (-1, 0),  # Left
+    (0, 1),  # Down
+]
+
+
+class Game(object):
+    """The game logic for Snake."""
+
+    def __init__(self, dims=(20, 20), snake_size=5):
+        """Constructor.
+
+        Args:
+            dims: A tuple of the dimensions of the game board
+            snake_size: The initial size of the snake
+        """
+        self._dims = dims
+        self.snake = self._generate_initial_snake(size=snake_size)
+        self.dir = (0, -1)
+        self.apple = self._generate_apple()
+
+    def _generate_initial_snake(self, size=5):
+        # TODO(EugenHotaj): snake should generate in random configurations.
+        snake = []
+        x = np.random.randint(0, self._dims[0] - size)
+        y = int(self._dims[1] / 2)
+        snake.append((x, y))
+        for _ in range(1, size):
+            x = x + 1
+            y = y
+            snake.append((x, y))
+        return snake
+
+    def step(self):
+        """Execute one step of the game."""
+        new_head = tuple(np.add(self.snake[-1], self.dir) % self._dims)
+        if new_head in self.snake:
+            return True
+
+        self.snake = self.snake[1:]
+        self.snake.append(new_head)
+
+        if new_head == self.apple:
+            # Append the new snake piece outside the game screen. It will be
+            # correctly placed on the next call to #update().
+            self.apple = self._generate_apple()
+            self.snake = [(-100, -100)] + self.snake
+
+        return False
+
+    def _generate_apple(self):
+        board = np.zeros(self._dims)
+        for x, y in self.snake:
+            board[x, y] = 1
+        empty = np.where(board == 0)
+        x = np.random.choice(empty[0])
+        y = np.random.choice(empty[0])
+        return x, y
+
 
 _WHITE = (255, 255, 255)
 _RED = (255, 0, 0)
@@ -24,9 +85,11 @@ _GREEN = (0, 255, 0)
 
 class SnakeEnv(gym.Env):
     """An environment which runs the game of Snake."""
+
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
+        """Constructor."""
         self._screen = None
         self._game = None
         self._done = False
@@ -39,6 +102,7 @@ class SnakeEnv(gym.Env):
         self.reset()
 
     def reset(self):
+        """Reset the game."""
         self._game = Game(dims=self._dims)
         self._done = False
 
@@ -55,10 +119,10 @@ class SnakeEnv(gym.Env):
         if self._done:
             logger.warn(('The environment has reached a terminal state and '
                          'should be reset (by calling env.reset()'))
-            return
+            return None
 
         if not self._game:
-            return
+            return None
 
         if not self.action_space.contains(action):
             action = 1
@@ -72,6 +136,8 @@ class SnakeEnv(gym.Env):
                 self._done,
                 {})
 
+    # TODO(ehotaj): remove close as an argument here and override the close
+    # method of gym.Env instead.
     def render(self, mode='human', close=False):
         """Only human mode is currently supported."""
         if not self._screen and not close:
