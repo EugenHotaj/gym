c22647316b7f0f78066272f8eb122922627d565d
Eugen Hotaj <eugen_hotaj_91@hotmail.com>
2018-05-03 22:38:24 -0400

    Fixed Bellman backup. Agent actually learns now

--- gym_snake_agent.py
+++ gym_snake_agent.py
@@ -17,11 +17,11 @@ class QFunctionApproxAgent(object):
         self._actions = actions
 
         self._gamma = 0.9
-        self._alpha = 0.0001
-        self._eps = 0.3
+        self._alpha = 0.1
+        self._eps = 1
 
         # bias + state + action
-        self._weights = np.zeros(1 + 5 + 3)
+        self._weights = np.random.uniform(0, 1, 13)
         self._prev_state_action = None
 
         self._step = 0
@@ -36,17 +36,32 @@ class QFunctionApproxAgent(object):
         left = state[(head[0], (head[1] - 1) % 10)]
         down = state[((head[0] + 1) % 10, head[1])]
 
+        # Has self close by
+        snake_right = 1 if right == 1 else 0
+        snake_up = 1 if up == 1 else 0
+        snake_left = 1 if left == 1 else 0
+        snake_down = 1 if down == 1 else 0
+        snake_one_hot = [snake_right, snake_up, snake_left, snake_down]
+
+        # Has apple close by
+        apple_right = 1 if right == 3 else 0
+        apple_up = 1 if up == 3 else 0
+        apple_left = 1 if left == 3 else 0
+        apple_down = 1 if down == 3 else 0
+        apple_one_hot = [apple_right, apple_up, apple_left, apple_down]
+
         # Distance to apple
         head = np.array([head[1][0], head[0][0]])
         apple = np.where(state == 3)
         apple = np.array([apple[1][0], apple[0][0]])
         dist = np.linalg.norm(head-apple)
 
-        one_hot_action = np.zeros(3)
-        one_hot_action[action] = 1
-
-        return np.concatenate(([1, dist, right, up, left, down],
-                               one_hot_action))
+        if action == 0:
+            return np.concatenate(([1], snake_one_hot, [0] * 8))
+        if action == 1:
+            return np.concatenate(([1], [0] * 4, snake_one_hot, [0] * 4))
+        if action == 2:
+            return np.concatenate(([1], [0] * 8, snake_one_hot))
 
     def _find_max_action(self, state):
         """Finds the action with the maximum expected reward in the `state`."""
@@ -54,9 +69,11 @@ class QFunctionApproxAgent(object):
         max_q_val = np.dot(self._weights, self._phi(state, max_action))
         for action in range(1, self._actions.n):
             q_val = np.dot(self._weights, self._phi(state, action))
+
             if q_val > max_q_val:
                 max_q_val = q_val
                 max_action = action
+
         return max_action
 
     def _update_q(self, next_state, reward, done):
@@ -66,14 +83,20 @@ class QFunctionApproxAgent(object):
         derivatives were done by hand.
         """
         if done:
-            q_prime = reward
+            reward = -10
         else:
-            phi_prime = self._phi(
-                next_state, self._find_max_action(next_state))
-            q_prime = reward + self._gamma * np.dot(self._weights, phi_prime)
+            reward = 1
+
         phi = self._phi(*self._prev_state_action)
-        self._weights -= (self._alpha *
-                          (q_prime - np.dot(phi, self._weights))) * phi
+        q = np.dot(self._weights, phi)
+        if done:
+            correction = reward
+        else:
+            phi_prime = self._phi(next_state,
+                                  self._find_max_action(next_state))
+            q_prime = np.dot(self._weights, phi_prime)
+            correction = reward + (self._gamma * q_prime) - q
+        self._weights += self._alpha * correction * phi
 
     def act(self, state, reward, done):
         """Updates the internal Q Function weights and choses an action.
@@ -87,31 +110,37 @@ class QFunctionApproxAgent(object):
             The next action to take.
         """
         self._step += 1
-        # TODO(ehotaj): decay alpha and eps
         if not self._prev_state_action:
             self._episode += 1
-            action = self._find_max_action(state)
-            self._prev_state_action = (state, action)
-            return action
-
-        self._cumulative_reward += reward
-        if (self._step % 1000 == 0):
-            print('Episode: %d. Avg Reward per 1000 steps: %f' %
-                  (self._episode, self._cumulative_reward / 100))
-            self._cumulative_reward = 0
-
-        self._update_q(state, reward, done)
+        else:
+            self._update_q(state, reward, done)
+            self._cumulative_reward += reward
 
         if done:
             self._prev_state_action = None
             return None
 
-        if np.random.uniform() < self._eps:
+        if not self._prev_state_action or np.random.uniform() < self._eps:
             action = self._actions.sample()
-            self._prev_state_action = (state, action)
-            return action
-        action = self._find_max_action(self._prev_state_action[0])
+        else:
+            action = self._find_max_action(self._prev_state_action[0])
+
+        # TODO(ehotaj): extract below commented sections into methods.
+        # TODO(ehotaj): decay alpha
+        # Anneal hyperparameters
         self._prev_state_action = (state, action)
+        if self._step < 100000:
+            self._eps -= 1/100000
+        if self._step > 100000:
+            FLAGS.render_mode = 'human'
+
+        # Report metrics
+        if (self._step % 1000 == 0):
+            print('Episode: %d. Avg Reward per 1000 steps: %f' %
+                  (self._episode, self._cumulative_reward / 100))
+            print('Eps: %f' % self._eps)
+            self._cumulative_reward = 0
+
         return action
 
 
@@ -122,6 +151,7 @@ def main(argv):
     agent = QFunctionApproxAgent(env.action_space, (10, 10))
     state, reward, done, _ = env.reset()
     while True:
+        env.render(FLAGS.render_mode)
         action = agent.act(state, reward, done)
         state, reward, done, _ = env.step(action) if not done else env.reset()
 
